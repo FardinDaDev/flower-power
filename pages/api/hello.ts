1e1f466db445cdef2b072f28ebdeac253e03734d
FardinDaDev <fardin0855@gmail.com>
2021-12-22 14:41:09 +0100

    example to check if mysql is working

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,1 +1,12 @@
-// WIP
+import { query } from "@lib/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const results = await query(`SELECT * FROM klant`);
+
+    return res.json(results);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+}
